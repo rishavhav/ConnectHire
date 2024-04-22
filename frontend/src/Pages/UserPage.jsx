67f06b8d722c741ed3cb38a868f40dcba60f47ef
Soam <rsoam@jcsinc.local>
2024-04-22 15:06:26 -0400

    user page updated

--- frontend/src/Pages/UserPage.jsx
+++ frontend/src/Pages/UserPage.jsx
@@ -1,10 +1,40 @@
+import { useEffect } from "react"
 import UserHeader from "../components/UserHeader"
 import UserPost from "../components/UserPost"
+import { useParams } from "react-router-dom"
+import useShowToast from "../hooks/useShowToast"
+import { useState } from "react"
 
 const UserPage = () => {
+  const { username } = useParams()
+  const showToasts = useShowToast()
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const response = await fetch(`/api/users/profile/${username}`)
+        const data = await response.json()
+        if (data.error) {
+          showToasts("Error", data.error, "error")
+          return
+        }
+
+        console.log(data)
+        setUser(data)
+      } catch (error) {
+        showToasts("Error", error, "error")
+      }
+    }
+
+    getUser()
+  }, [username, showToasts])
+
+  if (!user) return null
+
   return (
     <>
-      <UserHeader />
+      <UserHeader user={user} />
       <UserPost likesCount={219} commentsCount={3} postImage={"https://bit.ly/dan-abramov"} postTitle={"Hello HireConnect!"} />
       <UserPost likesCount={219} commentsCount={3} postTitle={"Hello HireConnect!"} />
       <UserPost likesCount={219} commentsCount={3} postImage={"https://bit.ly/dan-abramov"} postTitle={"Hello HireConnect!"} />
